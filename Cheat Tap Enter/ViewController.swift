3d98b8b1cffb582fa8c0485b9b85c279ed906cd4
Allen Welch <allenwelch@maed.local>
2017-10-10 12:54:58 -0600

    Initial Commit

---
+++ Cheat Tap Enter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cheat Tap Enter
+//
+//  Created by Allen Welch on 10/10/17.
+//  Copyright © 2017 Allen Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cheat Tap EnterTests/Cheat_Tap_EnterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cheat_Tap_EnterTests.swift
+//  Cheat Tap EnterTests
+//
+//  Created by Allen Welch on 10/10/17.
+//  Copyright © 2017 Allen Welch. All rights reserved.
+//
+
+import XCTest
+@testable import Cheat_Tap_Enter
+
+class Cheat_Tap_EnterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
